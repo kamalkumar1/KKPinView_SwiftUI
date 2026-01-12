d6d46b84ab3b8a2d74add93a026212baa670dfe8
kamal <kamalelango15@gmail.com>
2026-01-12 05:45:32 +0530

    fixed error message issue

--- KKPinView/Sources/KKPinView/KKPinViews.swift
+++ KKPinView/Sources/KKPinView/KKPinViews.swift
@@ -118,6 +118,7 @@ public struct KKPinViews: View {
             currentFieldIndex = 0
             errorMessage = nil
             updateLockoutStatus()
+            updateErrorMessage()
         }
     }
     
